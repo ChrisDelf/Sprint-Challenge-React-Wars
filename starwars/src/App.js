d9fd8c77aa5108e5c723b4f946c72f0e5641df97
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-07-19 12:08:46 -0400

    have components transfering the data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 import GridComp from './components/gridComp';
-import {Header, Table} from 'semantic-ui-react'
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -10,27 +10,28 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
- const [starWarsData, setStarWarsData] = useState([]);
+  const [starWarsData, setStarWarsData] = useState([]);
 
   useEffect(() => {
     axios
       .get(`https://swapi.co/api/people`)
       .then(response => {
-        console.log(response.data)
-        const starWarsResults = response.data
-        console.log("API data", starWarsResults.results)
-         setStarWarsData(starWarsResults.results)
-      })
+      console.log(response.data);
+      const starWarsResults = response.data;
+      console.log('API data', starWarsResults.results);
+      setStarWarsData(starWarsResults.results);
+    });
+  }, []);
 
 
-  },
-  [] )
+
+
   return (
     <div className="App">
-    <h1 className="Header">React Wars</h1>
-    <GridComp/>
+      <h1 className="Header">React Wars</h1>
+      <GridComp starWarsData= {starWarsData} key={starWarsData} />
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/gridComp.js
+++ starwars/src/components/gridComp.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import {Header, Table} from 'semantic-ui-react'
+import { Header, Table } from 'semantic-ui-react';
+import GridCard from './gridCard';
 
-function GridComp () {
 
-  return(
-    <Table>
-    <Header>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell singleLine>Name</Table.HeaderCell>
-        <Table.HeaderCell>Description</Table.HeaderCell>
-        <Table.HeaderCell>BirthDay</Table.HeaderCell>
-        <Table.HeaderCell>Year of Creation</Table.HeaderCell>
-        <Table.HeaderCell>Comments</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
-    </Header>
+
+
+
+
+function GridComp(props) {
+  const {starWarsData} = props;
+  console.log("Data in Grid", props)
+  return (
+    <Table celled padded>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell singleLine>Name</Table.HeaderCell>
+          <Table.HeaderCell>Description</Table.HeaderCell>
+          <Table.HeaderCell>BirthDay</Table.HeaderCell>
+          <Table.HeaderCell>Year of Creation</Table.HeaderCell>
+          <Table.HeaderCell>Comments</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        <GridCard />
+      </Table.Body>
     </Table>
- )
+  );
 }
 
-
-export default GridComp
+export default GridComp;
